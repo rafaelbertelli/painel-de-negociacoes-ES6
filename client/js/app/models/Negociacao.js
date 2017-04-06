6a635000a98e3180178fad8c6e59acdae048f2bf
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-04-05 21:32:10 -0300

    Commit feito a partir do VSCode

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,6 @@
 class Negociacao {
     
     constructor(data, quantidade, valor) {
-        
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
@@ -9,22 +8,18 @@ class Negociacao {
     }
     
     get volume() {
-        
         return this._quantidade * this._valor;
     }
     
     get data() {
-        
         return new Date(this._data.getTime());
     }
     
     get quantidade() {
-        
         return this._quantidade;
     }
     
     get valor() {
-        
         return this._valor;
     }
 }
